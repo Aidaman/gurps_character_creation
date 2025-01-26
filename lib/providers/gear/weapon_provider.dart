863505eb7c15177488fca54c9631433a13b49605
= <=>
2025-01-26 17:50:03 +0200

    feature: added weapon provider to eliminate weapon-related in CharacterProvider

---
+++ lib/providers/gear/weapon_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:gurps_character_creation/models/gear/weapons/weapon.dart';
+import 'package:gurps_character_creation/providers/character_provider.dart';
+import 'package:gurps_character_creation/services/gear/weapon_service.dart';
+
+class CharacterWeaponProvider with ChangeNotifier {
+  final CharacterProvider characterProvider;
+  final WeaponService weaponService;
+
+  CharacterWeaponProvider(this.characterProvider, this.weaponService);
+
+  List<Weapon> readAll() {
+    return weaponService.readAll(characterProvider.character);
+  }
+
+  Weapon read(String id) {
+    return weaponService.read(characterProvider.character, id);
+  }
+
+  void create(Weapon weapon) {
+    weaponService.create(characterProvider.character, weapon);
+
+    characterProvider.markDirty();
+    notifyListeners();
+  }
+
+  void update(Weapon newWeapon) {
+    weaponService.update(characterProvider.character, newWeapon);
+
+    characterProvider.markDirty();
+    notifyListeners();
+  }
+
+  void delete(String weaponId) {
+    weaponService.delete(characterProvider.character, weaponId);
+
+    characterProvider.markDirty();
+    notifyListeners();
+  }
+}
